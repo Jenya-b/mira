0d2e0467c049440fe0bc3bd29fdb9c318c0e6142
jenya-b <evgeny_rab@inbox.ru>
2024-05-08 18:57:04 +0300

    feat: change open app whith notification

--- public/sw2.js
+++ public/sw2.js
@@ -1,16 +1,28 @@
 function receivePushNotification(event) {
 	const message = JSON.parse(event.data.text());
 
-	event.waitUntil(
-		self.registration.showNotification(message.title, {
-			body: message.body,
-		})
-	);
+	const url = 'https://scintillating-empanada-mira.netlify.app/';
+
+	const notificationPromise = self.registration.showNotification(message.title, {
+		body: message.body,
+		data: {
+			url,
+		},
+	});
+
+	event.waitUntil(notificationPromise);
+
+	if (url && url !== 'null') {
+		event.waitUntil(clients.openWindow(url));
+	} else {
+		console.error('URL is not defined or is null:', url);
+	}
 }
 
 function openPushNotification(event) {
 	event.notification.close();
-	event.waitUntil(clients.openWindow(event.notification.data));
+	const url = event.notification.data.url;
+	event.waitUntil(clients.openWindow(url));
 }
 
 self.addEventListener('push', receivePushNotification);
